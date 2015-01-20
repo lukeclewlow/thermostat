305536f6ce6af10d10d558108e5d6af4eb3686ed
Luke Clewlow <luke.clewlow@gmail.com>
2015-01-20 17:05:42 +0000

    new failed test for minimum temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -13,7 +13,7 @@ describe ("Thermostat", function() {
     });
   });
 
-  describe("changing temperature", function() {
+  describe("the temperature", function() {
     it("can increase", function() {
       thermostat.increaseTemperature(2);
       expect(thermostat.temperature).toEqual(22);
@@ -24,23 +24,30 @@ describe ("Thermostat", function() {
       expect(thermostat.temperature).toEqual(18);
     });
 
+    it("can be set to a minimum of 10", function() {
+      thermostat.decreaseTemperature(11);
+      expect(thermostat.temperature).toBeGreaterThan(9);
+    });
+
   });
 
-  describe("you can change temperature", function() {
-    it ("by increasing it with the up button", function() {
-      thermostat.pressup();
-      expect(thermostat.temperature).toEqual(21);
-    });
-  });
+
+
+  // describe("you can change temperature", function() {
+  //   it ("by increasing it with the up button", function() {
+  //     thermostat.pressUp();
+  //     expect(thermostat.temperature).toEqual(21);
+  //   });
+  // });
 
 });
 
 // DONE... Thermostat starts at 20 degrees DONE. 
-// You can increase the temp with the up button
-// You can decrease the temp with the down button
 // The minimum temperature is 10 degrees
 // If power saving mode is on, the maximum temperature is 25 degrees
 // If power saving mode is off, the maximum temperature is 32 degrees
 // Power saving mode is on by default
 // You can reset the temperature to 20 by hitting the reset button
 // thermostat should color the display based on energy usage - < 18 is green, < 25 is yellow, otherwise red
+// You can increase the temp with the up button
+// You can decrease the temp with the down button
