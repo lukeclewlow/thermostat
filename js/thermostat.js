3239286f7be3a291600f6d522d2e5a30af3448e2
Jake Alvarez <jakealvarez81@hotmail.com>
2015-01-21 14:22:13 +0000

    thermostat built

--- js/thermostat.js
+++ js/thermostat.js
@@ -1,15 +1,53 @@
 var Thermostat = function() {
   this.temperature = 20;
+  this.minTemp = 10;
+  this.powerSaveMode = true;
+  this.maxTemp = 25;
 };
 
 Thermostat.prototype.increaseTemperature = function(changeTempBy) {
-  this.temperature += changeTempBy;
+  if(this.temperature + changeTempBy <= this.maxTemp) {
+    this.temperature += changeTempBy}
+  else {this.temperature = this.maxTemp}
 };
 
 Thermostat.prototype.decreaseTemperature = function(changeTempBy) {
-    if(this.temperature - changeTempBy > 10) {
-    console.log(this.temperature -= changeTempBy)}
-    else {console.log("error")}
+    if(this.temperature - changeTempBy > this.minTemp) {
+    this.temperature -= changeTempBy}
+    else {this.temperature = this.minTemp}
   };
 
+Thermostat.prototype.switchPowerSaveModeOff = function() {
+    this.powerSaveMode = false;
+    this.maxTemp = 32;
+  };
 
+Thermostat.prototype.switchPowerSaveModeOn = function() {
+  this.powerSaveMode = true;
+  this.maxTemp = 25;
+};
+
+Thermostat.prototype.resetButton = function() {
+  this.temperature = 20;
+};
+
+Thermostat.prototype.upButton = function() {
+  this.increaseTemperature(1)
+};
+
+Thermostat.prototype.downButton = function() {
+  this.decreaseTemperature(1)
+};
+
+Thermostat.prototype.tempDisplay = function() {
+  if(this.temperature <  18){
+    return "green"
+  }
+  else if(this.temperature >= 18 && this.temperature < 25){
+    return "yellow"
+  }
+  else{
+    return "red"
+  };
+
+};
--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -14,6 +14,7 @@ describe ("Thermostat", function() {
   });
 
   describe("the temperature", function() {
+
     it("can increase", function() {
       thermostat.increaseTemperature(2);
       expect(thermostat.temperature).toEqual(22);
@@ -29,25 +30,89 @@ describe ("Thermostat", function() {
       expect(thermostat.temperature).toBeGreaterThan(9);
     });
 
+    it("can not go above the maximum temperature", function() {
+      thermostat.increaseTemperature(14);
+      expect(thermostat.temperature).toEqual(thermostat.maxTemp);
+    });
+
+    it("can be reset to 20 by pressing reset button", function() { 
+      thermostat.temperature = 21;
+      thermostat.resetButton();
+      expect(thermostat.temperature).toEqual(20);
+    });
+
   });
 
+  describe("power saving mode", function() {
 
+    it("is switched on by default", function() {
+      expect(thermostat.powerSaveMode).toBe(true);
+     });
 
-  // describe("you can change temperature", function() {
-  //   it ("by increasing it with the up button", function() {
-  //     thermostat.pressUp();
-  //     expect(thermostat.temperature).toEqual(21);
-  //   });
-  // });
+    it("can be switched off", function() {
+      thermostat.switchPowerSaveModeOff();
+      expect(thermostat.powerSaveMode).toBe(false);
+    });
+
+    it ("sets maximum temperature to 25 when switched on", function() {
+      thermostat.switchPowerSaveModeOn();
+      expect(thermostat.maxTemp).toEqual(25);
+    });
+
+    it("sets maximum temperature to 32 when switched off", function() {
+      thermostat.switchPowerSaveModeOff();
+      expect(thermostat.maxTemp).toEqual(32);
+    });
+
+  });
+
+  describe ("using up and down buttons", function() {
+    
+    it("when the up button is pressed, the temperature increases by 1", function () {
+      thermostat.upButton();
+      expect(thermostat.temperature).toEqual(21);
+    });
+
+    it("when the down button is pressed, the temperature decreases by 1", function() {
+      thermostat.downButton();
+      expect(thermostat.temperature).toEqual(19);
+
+    });
+  });
+
+  describe ("colors of the display using energy usage", function() {
+
+    it("when energy usage is less than 18, display is green", function() {
+      thermostat.temperature = 17;
+      expect(thermostat.tempDisplay()).toEqual("green");    
+    });
+
+    it("when energy usage is less than 25, but more than or equal to 18, display is yellow", function() {
+      thermostat.temperature = 19;
+      expect(thermostat.tempDisplay()).toEqual("yellow");    
+    });
+
+        it("when energy usage is more than or equal to 25, display is red", function() {
+      thermostat.temperature = 27;
+      expect(thermostat.tempDisplay()).toEqual("red");    
+    });
+
+  });
+
+// .... <18 = green.... <25 yellow... >=25 red. 
 
 });
 
 // DONE... Thermostat starts at 20 degrees DONE. 
-// The minimum temperature is 10 degrees
-// If power saving mode is on, the maximum temperature is 25 degrees
-// If power saving mode is off, the maximum temperature is 32 degrees
-// Power saving mode is on by default
-// You can reset the temperature to 20 by hitting the reset button
-// thermostat should color the display based on energy usage - < 18 is green, < 25 is yellow, otherwise red
-// You can increase the temp with the up button
-// You can decrease the temp with the down button
+// DONE... The minimum temperature is 10 degrees
+// DONE... If power saving mode is on, the maximum temperature is 25 degrees
+// DONE... If power saving mode is off, the maximum temperature is 32 degrees
+// DONE... Power saving mode is on by default
+
+// DONE... You can reset the temperature to 20 by hitting the reset button
+
+// DONE... You can increase the temp with the up button
+// DONE... You can decrease the temp with the down button 
+// DONE... thermostat should color the display based on energy usage - < 18 is green, < 25 is yellow, otherwise red
+
+
